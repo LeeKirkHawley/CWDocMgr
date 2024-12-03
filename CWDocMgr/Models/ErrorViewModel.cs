87b9c5f670e3f08a81eb609088fc3d7a4cd1342c
Lee Kirk Hawley <leekirkhawley@gmail.com>
2024-12-03 10:32:07 -0700

    Add project files.

---
+++ CWDocMgr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CWDocMgr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
